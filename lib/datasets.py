ee01a9520a336f6151e703113f9d1137381ea394
diego-sacconi <diego-sacconi@users.noreply.github.com>
2017-07-04 14:24:22 +0200

    Add info about data format in dataset.py

--- lib/datasets.py
+++ lib/datasets.py
@@ -1,35 +1,52 @@
+'''
+The directories in data are structured as follows:
+    datasetname-|
+                |-datasetname.data
+                |-datasetname.graph
 
-#small traffic
-#Speeds from traffic sensors
-#vertices: 100
+   "datasetname.data" has info about the graph signal.
+    Each row has format:
+        "vertex_id,vertex_value"
+
+    "datasetname.graph" has info about edges.
+    If they are weighted each row has format:
+        "vertx_A,vertex_B,edge_weight"
+    Otherwise:
+        "vertex_A,vertex_B"
+'''
+
+
+# Small traffic (weighted)
+# Speeds from traffic sensors
+# Vertices: 100
 
 small_traffic = {}
 small_traffic["path"] = "data/small_traffic/"
 
-#Large traffic
-#Speeds from traffic sensors
-#vertices: 1923
+# Large traffic (weigthed)
+# Speeds from traffic sensors
+# Vertices: 1923
 
 traffic = {}
 traffic["path"] = "data/traffic/"
 
-#Human
-#Gene expression data
-#vertices: 3628
+# Human (unweighted)
+# Gene expression data
+# Vertices: 3628
 
 human = {}
 human["path"] = "data/human/"
 
-#Wikipedia data
-#Number of views of wikipedia pages
-#Vertices: 4871
+# Wikipedia data (unweighted)
+# Number of views of wikipedia pages
+# Vertices: 4871
 
 wiki = {}
 wiki["path"] = "data/wiki/"
 
-#Political blogs
-#Link network of congressman's blogs with democrat/republican (0/1) as signal
-#vertices: 1490
+# Political blogs (unweighted)
+# Link network of congressman's blogs with democrat/republican (0/1) as signal
+# Vertices: 1490
 
 polblogs = {}
 polblogs["path"] = "data/polblogs/"
